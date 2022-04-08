1767472e59dbfac3d9a3b0e24ddd7a5ea6323c61
mateusgalvao <mateusgalvao908@gmail.com>
2022-04-08 13:47:29 -0300

    terminando

---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+$("#add_user").submit(function(event){
+    alert("Dados inseridos com sucesso!");
+})
+
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function(n, i){
+        data[n['name']] = n['value']
+    })
+
+
+    var request = {
+        "url" : `http://localhost:3010/api/users/${data.id}`,
+        "method" : "PUT",
+        "data" : data
+    }
+
+    $.ajax(request).done(function(response){
+        alert("Dados atualizados com sucesso!");
+    })
+
+})
+
+if(window.location.pathname == "/"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url" : `http://localhost:3010/api/users/${id}`,
+            "method" : "DELETE"
+        }
+
+        if(confirm("Deseja mesmo excluir este registro?")){
+            $.ajax(request).done(function(response){
+                alert("Dados excluídos com sucesso");
+                location.reload();
+            })
+        }
+
+    })
+}
